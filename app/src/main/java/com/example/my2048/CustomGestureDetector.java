f7770943e275d0dea0a87d1f372c322fa32496d8
TECH06 <123>
2019-05-28 00:07:27 +0430

    add x and y compare

--- app/src/main/java/com/example/my2048/CustomGestureDetector.java
+++ app/src/main/java/com/example/my2048/CustomGestureDetector.java
@@ -25,21 +25,21 @@ public abstract class CustomGestureDetector extends GestureDetector.SimpleOnGest
     @Override
     public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX,
                            float velocityY) {
-        try {
+        if (Math.abs(e1.getX() - e2.getX()) > Math.abs(e1.getY() - e2.getY())) {
             if (e1.getX() - e2.getX() > SWIPE_MIN_DISTANCE
-                    && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
+                    && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY)
                 onLeftSwipe();
-            } else if (e2.getX() - e1.getX() > SWIPE_MIN_DISTANCE
-                    && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
+            else if (e2.getX() - e1.getX() > SWIPE_MIN_DISTANCE
+                    && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY)
                 onRightSwipe();
-            } else if (e1.getY() - e2.getY() > SWIPE_MIN_DISTANCE
-                    && Math.abs(velocityY) > SWIPE_THRESHOLD_VELOCITY) {
+
+        } else {
+            if (e1.getY() - e2.getY() > SWIPE_MIN_DISTANCE
+                    && Math.abs(velocityY) > SWIPE_THRESHOLD_VELOCITY)
                 onTopSwipe();
-            } else if (e2.getY() - e1.getY() > SWIPE_MIN_DISTANCE
-                    && Math.abs(velocityY) > SWIPE_THRESHOLD_VELOCITY) {
+            else if (e2.getY() - e1.getY() > SWIPE_MIN_DISTANCE
+                    && Math.abs(velocityY) > SWIPE_THRESHOLD_VELOCITY)
                 onDownSwipe();
-            }
-        } catch (Exception e) {
 
         }
         return false;
